2f2102e44f4e16fa8cfeab9296e25ba2f28c805c
Vnay12 <vnay1712@gmail.com>
2026-04-12 18:39:12 +0530

    DSA

--- src/Questions/StackQuestions/GreatestNearToRight.java
+++ src/Questions/StackQuestions/GreatestNearToRight.java
@@ -1,5 +1,6 @@
 /*
 This Code is for Greatest Near to Right Question
+LC : 496 - https://leetcode.com/problems/next-greater-element-i/description/
 Questions:
     1) We have given an array : 1 3 0 2
     2) we have to find the greatest element to arr[i]
@@ -10,7 +11,7 @@ Pattern recognition:
     1) If there is an array, and we get n^2 TC in our Brute approach and J loop is dependent on I then we use stack
 
 Implementation:
-    1) We will traverse array from left to right. why ? if we want to pop out / see the right element, it should be present at the top of stack ( LIFO )
+    1) We will traverse array from right to left. why ? if we want to pop out / see the right element, it should be present at the top of stack ( LIFO )
     2) On i'th element, we have to pushback all the right elements and on reaching end -1
     3) if stack[top] is greater than arr[i] then store it in o/p array
     4) if stack[top] is less than arr[i] then pop out
